6739c149428809484019323c841866eae1543b5b
Xnuiem <ryan.meinzer@xmtek.net>
2023-04-19 16:21:36 -0500

    fixing paths and coverage

--- backend/joblog/tests/tests.py
+++ backend/joblog/tests/tests.py
@@ -5,7 +5,7 @@ from backend.joblog import app
 
 
 cmd_folder = os.path.abspath(os.path.join(os.path.split(inspect.getfile(
-    inspect.currentframe()))[0], ".."))
+    inspect.currentframe()))[0], "../.."))
 if cmd_folder not in sys.path:
     sys.path.insert(0, cmd_folder)
 
