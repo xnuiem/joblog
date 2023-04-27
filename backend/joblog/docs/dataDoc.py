15c098f182ddfbb71eb715023c569f44c0861aaf
Xnuiem <ryan.meinzer@xmtek.net>
2023-04-27 11:48:48 -0500

    all cleaned up.  still need to finish the swagger

--- backend/joblog/docs/dataDoc.py
+++ backend/joblog/docs/dataDoc.py
@@ -8,6 +8,25 @@ class InitDataOut(Schema):
         metadata={'description': 'Return Message'}
     )
 
+class JobSchema(Schema):
+    pass
+
+jobDataExample = {
+    "id": "dc3f3d5d-888c-4d5e-bcb3-acce40a4e469",
+    "title": "VP of Engineering",
+    "status": "Applied",
+    "source": "Source",
+    "company": "Company",
+    "recruiter": "Recruiter",
+    "last_activity": "datetime",
+    "offer_date": "date",
+    "notes": "Notes",
+    "interviews": [{
+        "id": "date"
+    }
+    ]
+}
+
 
 dataOutExample = {
     'message': 'Data Init Complete'
